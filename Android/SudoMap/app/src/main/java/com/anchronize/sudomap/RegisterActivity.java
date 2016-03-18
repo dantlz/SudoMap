7d0fc54a9ecc12ba966aeff21f46fd116e04fd2c
Xinghan Wang <xinghanw@usc.edu>
2016-03-17 18:59:36 -0700

    1.Fix error in LoginActivity: New map view intent will be fired regardless of the result of user authentication. 2.Add RegisterActivity and modify LoginActivity

--- Android/SudoMap/app/src/main/java/com/anchronize/sudomap/RegisterActivity.java
+++ Android/SudoMap/app/src/main/java/com/anchronize/sudomap/RegisterActivity.java
@@ -3,20 +3,22 @@ package com.anchronize.sudomap;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.CursorLoader;
 import android.content.Intent;
-import android.content.Loader;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -41,7 +43,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class RegisterActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -65,7 +67,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
-    private Button mUsernameRegisterButton;
 
     // Maintain a connection to Firebase
     private Firebase ref;
@@ -73,7 +74,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         Firebase.setAndroidContext(this);
 
         // Set up the login form.
@@ -85,35 +86,24 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
+                    attemptRegister();
                     return true;
                 }
                 return false;
             }
         });
 
-        Button mUsernameSignInButton = (Button) findViewById(R.id.username_sign_in_button);
+        Button mUsernameSignInButton = (Button) findViewById(R.id.registerButton);
         mUsernameSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                attemptRegister();
             }
         });
 
-        mUsernameRegisterButton = (Button)findViewById(R.id.username_register);
-        mUsernameRegisterButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(i);
-            }
-        });
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
-
-
-
     }
 
 
@@ -167,7 +157,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
-    private void attemptLogin() {
+    private void attemptRegister() {
         if (mAuthTask != null) {
             return;
         }
@@ -274,7 +264,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 // Select only email addresses.
                 ContactsContract.Contacts.Data.MIMETYPE +
                         " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                                                                     .CONTENT_ITEM_TYPE},
+                .CONTENT_ITEM_TYPE},
 
                 // Show primary email addresses first. Note that there won't be
                 // a primary email address if the user hasn't specified one.
@@ -312,7 +302,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
         //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
         ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
+                new ArrayAdapter<>(RegisterActivity.this,
                         android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
         mUsernameView.setAdapter(adapter);
@@ -326,7 +316,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         private final String mUsername;
         private final String mPassword;
-        private boolean authenticationStatus = true;
+        private boolean registerStatus = false;
+
 
         UserLoginTask(String username, String password) {
             mUsername = username;
@@ -334,27 +325,23 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             doInBackground((Void) null);
             System.out.println(mUsername);
             System.out.println(mPassword);
-
-
         }
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            //A firebaseError will occur whenever the authentication fails.
+            // Note: Regardless of the result of regisration, an firebaseError will occur.
+            // However, OnSuccess is only called after a successful registration
             ref = new Firebase("https://anchronize.firebaseio.com");
-            ref.authWithPassword(mUsername, mPassword, new Firebase.AuthResultHandler() {
+            ref.createUser(mUsername, mPassword, new Firebase.ValueResultHandler<Map<String, Object>>() {
                 @Override
-                public void onAuthenticated(AuthData authData) {
-                    System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
-                    System.out.println(authData.getProviderData().get("email"));
+                public void onSuccess(Map<String, Object> result) {
+                    System.out.println("Successfully created user account with uid: " + result.get("uid"));
+                    registerStatus = true;
                 }
-
                 @Override
-                public void onAuthenticationError(FirebaseError firebaseError) {
-                    // there was an error
-                    System.out.println("fail to authenticate");
+                public void onError(FirebaseError firebaseError) {
+                    System.out.println("fail to register");
                     System.out.println(firebaseError.getMessage());
-                    authenticationStatus = false;
                     switch (firebaseError.getCode()) {
                         case FirebaseError.USER_DOES_NOT_EXIST:
                             System.out.println("USER_DOES_NOT_EXIST");
@@ -383,7 +370,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                     return pieces[1].equals(mPassword);
                 }
             }
-            return authenticationStatus;
+            return registerStatus;
 
         }
 
@@ -395,12 +382,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 finish();
-                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                 System.out.println(" onPostExecute success");
 
             } else {
                 System.out.println(" onPostExecute not success");
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.setError(getString(R.string.error_registration_fail));
                 mPasswordView.requestFocus();
             }
         }
