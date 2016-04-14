a486cb999f522445f684b0a65db1ec63f6dd40e9
Tianlin Zhang <mr.tianlinzhang@gmail.com>
2016-04-13 21:59:39 -0700

    EventDetailActivity has a brand new look

--- Android/SudoMap/app/src/main/java/com/anchronize/sudomap/EventDetailActivity.java
+++ Android/SudoMap/app/src/main/java/com/anchronize/sudomap/EventDetailActivity.java
@@ -5,7 +5,8 @@ import android.location.Geocoder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,7 +14,6 @@ import android.widget.Toast;
 import com.anchronize.sudomap.objects.EnumerationClasses;
 import com.anchronize.sudomap.objects.Event;
 import com.anchronize.sudomap.objects.User;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,7 +22,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -31,10 +30,12 @@ public class EventDetailActivity extends AppCompatActivity implements
         OnMapReadyCallback,
         ActivityCompat.OnRequestPermissionsResultCallback{
 
+    private TextView titleView;
     private TextView locationNameView;
     private TextView locationAddress;
-    private HorizontalScrollView attendantsScrollView;
     private TextView descriptionView;
+    private HorizontalScrollView attendantsScrollView;
+    private Button chatButton;
 
     private Event mEvent;
     private GoogleMap mMap;
@@ -44,10 +45,12 @@ public class EventDetailActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
 
+        titleView = (TextView) findViewById(R.id.eventTitle);
         locationNameView = (TextView) findViewById(R.id.eventLocationNameTextView);
         locationAddress = (TextView) findViewById(R.id.eventLocationAddressTextView);
-        attendantsScrollView = (HorizontalScrollView) findViewById(R.id.attendantsScrollView);
         descriptionView = (TextView) findViewById(R.id.eventDescriptionView);
+        attendantsScrollView = (HorizontalScrollView) findViewById(R.id.attendantsScrollView);
+        chatButton = (Button) findViewById(R.id.chatButton);
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.eventsMap);
@@ -61,15 +64,15 @@ public class EventDetailActivity extends AppCompatActivity implements
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-//        Toast.makeText(EventDetailActivity.this, "MAP READY", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(EventDetailActivity.this, "MAP READY", Toast.LENGTH_SHORT).show();
 
         mMap = googleMap;
 
         //Pass in an event from HomeActivity by calling setEvent.
         //Delete the hardcoded event below
         mEvent = new Event("TEST");
-        mEvent.setTitle("TEST title");
-        mEvent.setDescription("TEST description");
+        mEvent.setTitle("Dope Event");
+        mEvent.setDescription("THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING THIS IS MEANT TO BE A SUPER LONG STRING ");
         mEvent.setCategory(EnumerationClasses.Categories.FUN.toString());
         mEvent.setLongitude(-118.279838);
         mEvent.setLatitude(34.022799);
@@ -82,17 +85,28 @@ public class EventDetailActivity extends AppCompatActivity implements
 
     public void populateDetails(){
 
+        titleView.setText(mEvent.getTitle());
+        locationNameView.setText(nameFromLatLng(mEvent.getLatitude(),mEvent.getLongitude()));
+        locationAddress.setText(addressFromLatLng(mEvent.getLatitude(), mEvent.getLongitude()));
+        descriptionView.setText(mEvent.getDescription());
         mMap.addMarker(new MarkerOptions().position(new
                 LatLng(mEvent.getLatitude(), mEvent.getLongitude())).title("Hello world"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mEvent.getLatitude(), mEvent.getLongitude()),15));
-
-        locationNameView.setText("UTAGYDHIUS");
-        locationAddress.setText(addressFromLatLng(mEvent.getLatitude(), mEvent.getLongitude()));
-
-        descriptionView.setText(mEvent.getDescription());
+        //Populate the attendants horizontal scroll view | Tinder scrolling
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chatButtonClicked();
+            }
+        });
     }
 
-    //Return a string
+    public void chatButtonClicked(){
+        Toast.makeText(EventDetailActivity.this, "Chat Button Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+
+
     public String addressFromLatLng(double lat, double lng){
         Geocoder geocoder;
         List<Address> addresses;
@@ -106,6 +120,21 @@ public class EventDetailActivity extends AppCompatActivity implements
         }
 
         return "RETRIEVE ADDRESS FAILED";
+    }
+
+    public String nameFromLatLng(double lat, double lng){
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(lat, lng, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            return addresses.get(0).getFeatureName(); // Only if available else return NULL
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "RETRIEVE ADDRESS NAME FAILED";
     }
 
     @Override
