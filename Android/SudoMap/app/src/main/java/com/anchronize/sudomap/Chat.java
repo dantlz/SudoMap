8f8d388aa2ecd718653038e8706937baa3f66063
Tianlin Zhang <mr.tianlinzhang@gmail.com>
2016-04-14 00:23:48 -0700

    Chat files added

---
+++ Android/SudoMap/app/src/main/java/com/anchronize/sudomap/Chat.java
@@ -0,0 +1,35 @@
+package com.anchronize.sudomap;
+
+/**
+ * Created by tianlinz on 4/14/16.
+ */
+public class Chat {
+
+    private String message;
+    private String author;
+    private int votes;
+
+    // Required default constructor for Firebase object mapping
+    @SuppressWarnings("unused")
+    private Chat() {
+    }
+
+    Chat(String message, String author) {
+        this.message = message;
+        this.author = author;
+        this.votes = 0;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
