3c15aacc5001ba5394c6c9369dd7962ebde95826
glarence <glarencez@gmail.com>
2016-04-15 20:51:12 -0700

    Pushing YourEventActivity classes and layouts

---
+++ Android/SudoMap/app/src/main/java/com/anchronize/sudomap/navigationdrawer/TabAdapter.java
@@ -0,0 +1,42 @@
+package com.anchronize.sudomap.navigationdrawer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.anchronize.sudomap.R;
+import com.anchronize.sudomap.objects.Event;
+
+import java.util.ArrayList;
+
+/**
+ * Created by glarencezhao on 4/14/16.
+ */
+public class TabAdapter extends ArrayAdapter<Event> {
+
+    private Context context;
+
+    public TabAdapter(Context context, ArrayList<Event> eventArray){
+        super(context, 0, eventArray);
+
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Event event = getItem(position);
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_yourevent_fragments, parent, false);
+        }
+
+        TextView eventTitleTV = (TextView) convertView.findViewById(R.id.eventTitleTextView);
+
+        eventTitleTV.setText(event.getTitle());
+
+        return convertView;
+    }
+}
