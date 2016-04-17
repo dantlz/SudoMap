f729fa29909a66b168b66d3a4512e84ff8eef0d2
Tianlin  Zhang <mr.tianlinzhang@gmail.com>
2016-04-17 08:01:59 +0000

    Override G's EventDetailActivity with master/Sean's because G's is aesthetic changes

--- Android/SudoMap/app/src/main/java/com/anchronize/sudomap/EventDetailActivity.java
+++ Android/SudoMap/app/src/main/java/com/anchronize/sudomap/EventDetailActivity.java
@@ -13,9 +13,14 @@ import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.anchronize.sudomap.objects.Event;
 import com.anchronize.sudomap.objects.User;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,15 +29,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class EventDetailActivity extends AppCompatActivity implements
         GoogleMap.OnMyLocationButtonClickListener,
         OnMapReadyCallback,
         ActivityCompat.OnRequestPermissionsResultCallback{
     //A unique keyname, so that mainActivity can use this key and pass selected event to this activity
-    public static final String EVENT_KEY = "com.anchronize.sudomap.EventDetailActivity.event";
+    public static final String EVENT_KEY = "com.anchronize.sudomap.EventDetailActivity.eventKEY";
+    public static final String EVENTID_KEY = "com.anchronize.sudomap.EventDetailActivity.eventIDKEY";
 
     private TextView titleView;
     private TextView organizerView;
@@ -41,16 +50,18 @@ public class EventDetailActivity extends AppCompatActivity implements
     private TextView descriptionView;
     private HorizontalScrollView attendantsScrollView;
     private LinearLayout attendantsView;
+    private Button chatButton;
     private Button bookmarkButton;
     private Button attendingButton;
 
     private Event mEvent;
     private GoogleMap mMap;
+    private Firebase ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_detail_updated);
+        setContentView(R.layout.activity_event_detail);
         initializeComponents();
         addListeners();
     }
@@ -58,14 +69,12 @@ public class EventDetailActivity extends AppCompatActivity implements
     public void initializeComponents(){
         //Current Event
         Intent i = getIntent();
-        //mEvent = (Event)i.getSerializableExtra(EVENT_KEY);
-        //TESTING
-        mEvent = getTestEvent();
+        mEvent = (Event)i.getSerializableExtra(EVENT_KEY);
 
         //Map
-        /*SupportMapFragment mapFragment =
+        SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.eventsMap);
-        mapFragment.getMapAsync(this);*/
+        mapFragment.getMapAsync(this);
 
         //GUI
         titleView = (TextView) findViewById(R.id.eventTitle);
@@ -75,11 +84,18 @@ public class EventDetailActivity extends AppCompatActivity implements
         descriptionView = (TextView) findViewById(R.id.eventDescriptionView);
         attendantsScrollView = (HorizontalScrollView) findViewById(R.id.attendantsScrollView);
         attendantsView = (LinearLayout) findViewById(R.id.attendants);
+        chatButton = (Button) findViewById(R.id.chatButton);
         bookmarkButton = (Button) findViewById(R.id.bookmarkButton);
         attendingButton = (Button) findViewById(R.id.attendingButton);
     }
 
     public void addListeners(){
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chatButtonClicked();
+            }
+        });
         bookmarkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,57 +112,127 @@ public class EventDetailActivity extends AppCompatActivity implements
 
     public void chatButtonClicked(){
         Intent i = new Intent(this, ChatActivity.class);
+        i.putExtra(ChatActivity.EVENTID_KEY,mEvent.getEventID());
+        i.putExtra(ChatActivity.EVENTDESC_KEY, mEvent.getDescription());
+        i.putExtra(ChatActivity.USERNAME_KEY, ((SudoMapApplication) getApplication()).getCurrentUser().getInAppName());
         startActivity(i);
     }
 
     public void bookmarkButtonClicked(){
-
+        //get current user and update bookmarked events
         User user = ((SudoMapApplication)getApplication()).getCurrentUser();
+        if(user.getBookmarkedEventIDs().contains(mEvent.getEventID())){
+            return;         //if user already bookmark this event, do nothing
+        }
         user.addBookmarkedEventID(mEvent.getEventID());
-        ((SudoMapApplication)getApplication()).updateCurrentUser(user);
+
+        //update the firebase
+        Firebase refUser = ref.child("users").child(user.getUserID());
+        Map<String, Object> eventBookmarked = new HashMap<String, Object>();
+        eventBookmarked.put("bookmarkedEventIDs", user.getBookmarkedEventIDs());
+        refUser.updateChildren(eventBookmarked);
+
     }
 
     public void attendingButtonClicked(){
+        //get current user and update attending events
         User user = ((SudoMapApplication)getApplication()).getCurrentUser();
+        if(user.getAttendingEventIDs().contains(mEvent.getEventID())){
+            return;         //if user already attend this event, do nothing
+        }
         user.addAttendingEventID(mEvent.getEventID());
-        ((SudoMapApplication)getApplication()).updateCurrentUser(user);
-        mEvent.addAttendant(user.getUserID());
-        //TODO update this event in firebase
+
+
+        //update the firebase - user part
+        Firebase refUser = ref.child("users").child(user.getUserID());
+        Map<String, Object> eventAttending = new HashMap<String, Object>();
+        eventAttending.put("attendingEventIDs", user.getAttendingEventIDs());
+        refUser.updateChildren(eventAttending);
+
+        //update the firebases - event part
+        Firebase refEvent = ref.child("events").child(mEvent.getEventID());
+        ArrayList<String> attendentIDs = mEvent.getattendantsID();
+        if(!attendentIDs.contains(user.getUserID())){
+            //update the attendentIDs array
+            attendentIDs.add(user.getUserID());
+            Map<String, Object> attendee = new HashMap<String, Object>();
+            attendee.put("attendantsID", attendentIDs);
+            refEvent.updateChildren(attendee);
+        }
+
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        //populateDetails();
+        populateDetails();
     }
 
     public void populateDetails(){
 
+        //get Organizer from Firebase, set organizer's name to textView
+        ref = new Firebase("https://anchronize.firebaseio.com");
+        Firebase refOrganizer = ref.child("users").child(mEvent.getOrganizerID());
+        refOrganizer.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User organizer = dataSnapshot.getValue(User.class);
+                //update the organizer text view here
+                organizerView.setText(organizer.getInAppName());
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
+
         //TODO category
+
         titleView.setText(mEvent.getTitle());
-        //TODO getUserFromID seems to be broken
-        User organizer = (((SudoMapApplication)getApplication()).getUserFromID(mEvent.getOrganizerID()));
-        String organizerName = "";
-        if(organizer == null){
-            organizerName = "null - getUserFromID() returned null bro";
-        }
-        else{
-            organizerName = organizer.getInAppName();
-        }
-        organizerView.setText("Organizer: "+ organizerName);
-        locationNameView.setText(nameFromLatLng(mEvent.getLatitude(),mEvent.getLongitude()));
-        locationAddress.setText(addressFromLatLng(mEvent.getLatitude(), mEvent.getLongitude()));
+        locationNameView.setText(mEvent.getAddressName());
+        locationAddress.setText(mEvent.getAddress());
         descriptionView.setText(mEvent.getDescription());
         mMap.addMarker(new MarkerOptions().position(new
                 LatLng(mEvent.getLatitude(), mEvent.getLongitude())).title("Hello world"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mEvent.getLatitude(), mEvent.getLongitude()),15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mEvent.getLatitude(), mEvent.getLongitude()), 15));
 
+
+        Toast.makeText(getApplicationContext(), ((SudoMapApplication) getApplication()).getCurrentUser().getInAppName(), Toast.LENGTH_LONG).show();
 //        for(User user: mEvent.getAttendants()){
 //            AttendantsItem item = new AttendantsItem(getApplicationContext());
 //            item.setName(user.getInAppName());
 //            item.setPicBitMap(user.getProfileImageBitMap());
 //            attendantsView.addView(item);
 //        }
+
+//        for(String userID: mEvent.getAttendants()){
+//            Firebase attendeeRef = ref.child("users").child(userID);
+//            attendeeRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(DataSnapshot dataSnapshot) {
+//                    User attendee = dataSnapshot.getValue(User.class);
+//
+//                    AttendantsItem item = new AttendantsItem(getApplicationContext());
+//                    item.setName(attendee.getInAppName());
+//
+//                    Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.miller);
+//                    item.setPicBitMap(bitmap);
+//                    attendantsView.addView(item);
+//
+//                }
+//
+//                @Override
+//                public void onCancelled(FirebaseError firebaseError) {
+//
+//                }
+//            });
+//
+//        }
+
         for(int i = 0; i < 5; i++){
             AttendantsItem item = new AttendantsItem(getApplicationContext());
             item.setName(Integer.toString(i)+ " -test- " + Integer.toString(i));
@@ -189,28 +275,5 @@ public class EventDetailActivity extends AppCompatActivity implements
     @Override
     public boolean onMyLocationButtonClick() {
         return false;
-    }
-
-    private Event getTestEvent(){
-        Event event = new Event();
-        event.setTitle("Test Title");
-        event.setDescription("Testing this event + \n description 123 \n suhhhh dude!!! \n fuck you");
-        event.setCategory("FUN");
-        event.setAddress("2428 Santa Monica Blvd");
-        event.setLongitude(0.0);
-        event.setLatitude(0.0);
-        event.setVisible(true);
-        event.setStartDay(25);
-        event.setStartMonth(3);
-        event.setStartYear(2016);
-        event.setStartHour(14);
-        event.setStartMinute(0);
-        event.setEndDay(26);
-        event.setEndMonth(3);
-        event.setEndYear(2016);
-        event.setEndHour(2);
-        event.setEndMinute(9);
-
-        return event;
     }
 }
